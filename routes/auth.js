3b2068f657b3e58845cf8cac10ccf6e84014b0ff
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-08-25 16:53:37 +0100

    Test for Test user in Auth middleware and restrict CRUD functionality

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authenticateUser = require('../middleware/authentication');
 const {register, login, updateUser} = require('../controllers/auth');
+const testUser = require('../middleware/testUser');
 
 router.post('/register', register);
 router.post('/login', login);
-router.patch('/updateUser', authenticateUser, updateUser);
+router.patch('/updateUser', authenticateUser, testUser, updateUser);
 
 module.exports = router;
